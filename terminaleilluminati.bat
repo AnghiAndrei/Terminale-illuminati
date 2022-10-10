f74043a8914f108004b5aa8ff99745f24e12da28
AnghiAndrei <115498826+AnghiAndrei@users.noreply.github.com>
2022-10-10 20:48:36 +0200

    Add files via upload

---
+++ terminaleilluminati.bat
@@ -0,0 +1,263 @@
+@echo off
+cls
+color C
+title TOP SECRET 
+SET password=sbagliata
+SET mone=Angly
+                                   :avvio
+@echo off
+cls
+color C
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW     O WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW      O  WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW    O        WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW     O         WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW  O         O      WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   O         O       WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWW           O          O  WWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWW           MMM         O   WWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOOOMMM   MMMOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWW    O   MM         MMO         WWWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWW     O MM     OOO       MM       WWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOM       OOO         MOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWW O       MM             MM       O   WWWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWW  O         MMM       MMM         O    WWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWOOOOOOOOOOOOOOOOMMMMMMMOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWW          O                   O            WWWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWW           O                   O             WWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWW  O                   O                   O      WWWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWW   O                   O                   O       WWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWW
+echo WWWWWWWWWWWWWW          O                           O                WWWWWWWWWWWWWW
+echo WWWWWWWWWWWWW           O                           O                 WWWWWWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWANGLYWWWWWWWW
+echo WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWREGNAWWWW
+echo. 
+echo                         ! ATTENZIONE !
+echo ogni attivita' sul seguente terminale e' monitorata dagli
+echo.
+echo                      ILLUMINATI
+echo .
+echo premere s per continuare o n per uscire
+SET/p "ww=>"
+if %ww%==s goto 4
+if %ww%==n goto end1
+if %ww%==S goto 4
+if %ww%==N goto end1
+pause
+goto avvio
+:4
+cls
+@echo off
+color C
+echo -------------------
+echo.
+echo Identificazione...
+echo.
+echo.
+echo -------------------
+SET/p "ass=>"
+if %ass%==%mone% goto 1
+echo Utente non riconosciuto
+echo. 
+echo L illumati sanno del tuo fallimento 
+goto virus 
+:1
+@echo off
+cls
+color C
+echo --------------------
+echo.
+echo Password d accesso
+echo.
+echo --------------------
+SET/p "as=>"
+if %as%==%password% goto q
+echo La password e sbagliata 
+echo.
+echo 2 tentativi rimasti
+pause
+goto 2
+
+:2
+@echo off
+cls 
+color C
+echo --------------------
+echo.
+echo Password d accesso
+echo.
+echo --------------------
+SET/p "as=>"
+if %as%==%password% goto q
+echo La password e sbagliata 
+echo.
+echo 1 tentativi rimasti
+pause
+goto 3
+ :3
+@echo off
+cls 
+color C
+echo --------------------
+echo.
+echo Password d accesso
+echo.
+echo --------------------
+SET/p "as=>"
+if %as%==%password% goto q
+echo La password e sbagliata 
+echo.
+echo 0 tentativi rimasti
+goto virus 
+:q
+@echo off
+cls
+color C
+echo --------------------------
+echo.
+echo.
+echo o
+echo.
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo to
+echo.
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo uto
+echo ?
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo nuto
+echo lei?
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo enuto
+echo per lei?
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo venuto
+echo fare per lei?
+echo.
+echo --------------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo nvenuto
+echo posso fare per lei?
+echo.
+echo -------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.
+echo envenuto
+echo cosa possso fare per lei?
+echo.
+echo -------------------
+PING localhost -n10>nul
+cls
+echo --------------------------
+echo.
+echo.                     
+echo Benvenuto               
+echo cosa posso fare per lei?                                  
+echo. 
+echo.
+echo --------------------------
+SET/p "qq=>"
+if %qq%== help goto w
+if %qq%== info goto e
+if %qq%== chiudi goto end2
+
+echo li comando non e stato riconosciuto
+echo.
+echo                help
+echo.
+echo      per la lista dei comandi
+echo.
+echo.
+pause
+goto q
+     
+:w
+@echo off
+cls
+color C
+echo.
+echo chisei? = info
+echo.
+echo chiudi terminale = chiudi
+echo.
+echo premere un qualsiasi tasto per tornare al menu principale
+pause>nul
+goto q
+:e
+@echo off
+cls
+color C
+echo ! RICORDA OGNI ATTIVITA CON SEGUENTE TERMINALE E CONTROLLATA DAI ILLUMINATI !
+echo. 
+echo                         Terminale dei Illuminati 
+echo.
+echo creato da Angly
+echo. 
+echo il 26-10-2019
+echo.
+echo.
+echo premere un qualsiasi tasto per tornare al menu principale
+pause>nul
+goto q
+:end2
+cls
+echo chiusura terminale in corso...
+PING locate -n90>nul
+goto end
+:end1
+cls
+echo saggia scelta 
+echo ma ormai e troppo tardi
+echo sai gia troppo
+pause 
+end
+:virus
+start C:ti\avevamo\avvisato 
+goto virus
+:end
+end
